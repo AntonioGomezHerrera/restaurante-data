2cc9849a420be1c5ed7e8011af4d65ae4ec4c97a
Nokster <noksterdj@gmail.com>
2021-01-17 02:49:54 -0600

    Generando setters y getters en clase Menu

--- restaurante-data/src/com/nokster/restaurantedata/entity/Menu.java
+++ restaurante-data/src/com/nokster/restaurantedata/entity/Menu.java
@@ -20,4 +20,40 @@ public class Menu {
 	 * Descripcion del menu
 	 */
 	private String descripcion;
+	/**
+	 * @return the idMenu
+	 */
+	public int getIdMenu() {
+		return idMenu;
+	}
+	/**
+	 * @param idMenu the idMenu to set
+	 */
+	public void setIdMenu(int idMenu) {
+		this.idMenu = idMenu;
+	}
+	/**
+	 * @return the clave
+	 */
+	public String getClave() {
+		return clave;
+	}
+	/**
+	 * @param clave the clave to set
+	 */
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+	/**
+	 * @return the descripcion
+	 */
+	public String getDescripcion() {
+		return descripcion;
+	}
+	/**
+	 * @param descripcion the descripcion to set
+	 */
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
 }

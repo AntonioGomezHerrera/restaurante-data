c85a62aa525e57437e9a6228f9b011b0f77e0d6f
Nokster <noksterdj@gmail.com>
2021-01-17 16:01:23 -0600

    Clase que implementa el CRUD y otros metodos personalizados de transacciones a la base de datos en la tabla tipo_restaurante

---
+++ restaurante-data/src/com/nokster/restaurantedata/dao/impl/TipoRestauranteDAOImpl.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package com.nokster.restaurantedata.dao.impl;
+
+import java.util.List;
+
+import com.nokster.restaurantedata.dao.TipoRestauranteDAO;
+import com.nokster.restaurantedata.entity.TipoRestaurante;
+
+/**
+ * @author Nokster
+ * Clase que implementa el CRUD y otros metodos personalizados de transacciones a la base de datos en la tabla
+ * tipo_restaurante
+ */
+public class TipoRestauranteDAOImpl implements TipoRestauranteDAO{
+
+	@Override
+	public int guardar(TipoRestaurante tipoRestaurante) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int actualizar(TipoRestaurante tipoRestaurante) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int eliminar(int idTipoRestaurante) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<TipoRestaurante> consultar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TipoRestaurante consultarPorId(int idTipoRestaurante) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

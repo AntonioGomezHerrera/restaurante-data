31342881d1385ea6324ca383365817bbbe313ab9
Nokster <noksterdj@gmail.com>
2021-01-17 16:02:23 -0600

    Creando metodos ejecutarSQL y ejecutarSQLSelect

--- restaurante-data/src/com/nokster/restaurantedata/conecction/ConnectionFactory.java
+++ restaurante-data/src/com/nokster/restaurantedata/conecction/ConnectionFactory.java
@@ -5,7 +5,9 @@ package com.nokster.restaurantedata.conecction;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * @author Nokster Clase que se encarga de realizar la configuracion, la
@@ -17,6 +19,11 @@ public class ConnectionFactory {
 	 * Objeto encargado de mantener la conexion de la bd
 	 */
 	private static Connection connection;
+	
+	/**
+	 * Objeto encargado de habilitar y ejecutar las sentencias SQL
+	 */
+	private static Statement statement;
 
 	/**
 	 * Metodo que permite conectarse a la bd
@@ -38,7 +45,29 @@ public class ConnectionFactory {
 
 		// Paso 3: Establecer la conexion
 		connection = DriverManager.getConnection(url, user, password);
+		statement = connection.createStatement(); // Habilitando proceso para ejecutar sentencias SQL
 
 		return connection;
+	}
+	/**
+	 * Permite ejecutar sentencias Insert, Update y Delete
+	 * @param sql parametro con la sentencia a ejecutarse
+	 * @return 1 en caso de ser exitoso, 0 en caso de error
+	 * @throws SQLException Excepcion generada en caso de error al ejecutar la senencia SQL
+	 */
+	public static int ejecutarSQL(String sql) throws SQLException {
+		System.out.println("Query: " + sql);
+		return statement.executeUpdate(sql);
+	}
+	
+	/**
+	 * Permite ejecutar sentencias Select
+	 * @param sql parametro con la sentencia a ejecutarse
+	 * @return objeto con la informacion obtenida de la sentencia Select
+	 * @throws SQLException Excepcion generada en caso de error al ejecutar la senencia SQL
+	 */
+	public static ResultSet ejecutarSQLSelect(String sql) throws SQLException {
+		System.out.println("Query: " + sql);
+		return statement.executeQuery(sql);
 	}
 }

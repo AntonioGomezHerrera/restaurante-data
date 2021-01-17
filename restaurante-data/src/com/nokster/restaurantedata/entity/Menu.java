68a67a3d81decfd98577e1d15e781fa5a50856f9
Nokster <noksterdj@gmail.com>
2021-01-17 02:43:21 -0600

    Creando clase Menu para mapeo en DB

---
+++ restaurante-data/src/com/nokster/restaurantedata/entity/Menu.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.nokster.restaurantedata.entity;
+
+/**
+ * @author Nokster
+ * Clase que representa una entidad de menu de la bd
+ */
+public class Menu {
+	/**
+	 * Identificador del menú
+	 */
+	private int idMenu;
+	/**
+	 * Clave del menú
+	 */
+	private String clave;
+	/**
+	 * Descripcion del menu
+	 */
+	private String descripcion;
+}
